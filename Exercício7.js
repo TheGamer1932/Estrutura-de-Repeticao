52659467cf8602606b9bd56a988521b58969df45
Tiago Ferreira da Silva <132114628+TheGamer1932@users.noreply.github.com>
2023-06-05 23:53:23 -0300

    Exercício7.js

---
+++ Exercício7.js
@@ -0,0 +1,9 @@
+const pessoas = [
+  { nome: "João", idade: 30 },
+  { nome: "Maria", idade: 25 },
+  { nome: "Pedro", idade: 40 },
+];
+
+for (let i = 0; i < pessoas.length; i++) {
+  console.log(pessoas[i].nome);
+}
